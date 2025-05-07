47eee1421903b667ec8b6db09141dda8cdddd499
craigb20 <157324902+craigb20@users.noreply.github.com>
2025-05-07 10:48:01 -0400

    Changed coordinates on the first playmap back for demo

--- pokete_data/map_data.py
+++ pokete_data/map_data.py
@@ -152,12 +152,12 @@ map_data = {
             "dor_playmap_51_1": {
                 "x": 90,
                 "y": 12,
-                "args": {"map": "playmap_57", "x": 1, "y": 28}
+                "args": {"map": "playmap_51", "x": 1, "y": 28}
             },
             "dor_playmap_51_2": {
                 "x": 90,
                 "y": 13,
-                "args": {"map": "playmap_57", "x": 1, "y": 29}
+                "args": {"map": "playmap_51", "x": 1, "y": 29}
             },
         },
         "balls": {
